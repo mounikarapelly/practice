9e528ebfc8bd02d18ac5764413d22e5119c7448e
mounikarapelly <99536475+mounikarapelly@users.noreply.github.com>
2022-06-13 14:33:38 +0530

    Add files via upload

---
+++ junittest/src/junittest/JunitTest.java
@@ -0,0 +1,7 @@
+package junittest;
+
+import junit.framework.TestCase;
+
+public class JunitTest extends TestCase {
+
+}
---
+++ junittest/src/junittest1/MainTest.java
@@ -0,0 +1,13 @@
+package junittest1;
+
+public class MainTest {
+	public static void main(String[] args) {
+		
+		Account a1=new Account("1501","sreee");
+		Account a2=new Account("1082","charan",10000);
+		System.out.println(a1);
+		System.out.println(a2);
+
+
+		}
+}
